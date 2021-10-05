f7ebee902d39dc56269636c4f4ab6b176cc9337a
kimsuyeon <81313960+Suyeon9911@users.noreply.github.com>
2021-10-06 07:58:15 +0900

    ✨[FEAT] 회원가입 화면 전환 , 비밀번호 표시 기능 구현

--- 29th-Assignment/29th-Assignment-Youtube/29th-Assignment-Youtube/SignUpViewController.swift
+++ 29th-Assignment/29th-Assignment-Youtube/29th-Assignment-Youtube/SignUpViewController.swift
@@ -8,22 +8,69 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
+    
+    // var iconClick = true
 
+    // MARK: IBOutlets
+    
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var nextButton: UIButton!
+    @IBOutlet var showPasswordButton: UIButton!
+    
+    
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        nextButton.isEnabled = false
+        
+        self.nameTextField.addTarget(self, action: #selector(self.checkFilledTextField(_:)), for: .editingChanged)
+        self.emailTextField.addTarget(self, action: #selector(self.checkFilledTextField(_:)), for: .editingChanged)
+        self.passwordTextField.addTarget(self, action: #selector(self.checkFilledTextField(_:)), for: .editingChanged)
+    
         // Do any additional setup after loading the view.
     }
+    // MARK: - Methods
+    // MARK: Custom Method
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func checkFilledTextField(_ sender: Any?) {
+        
+        if nameTextField.text != "" && emailTextField.text != "" && passwordTextField.text != "" {
+            nextButton.isEnabled = true
+        } else {
+            nextButton.isEnabled = false
+        }
+        
     }
-    */
+   
+    
+    // MARK: IBActions
+    
+    @IBAction func showPasswordButton(_ sender: UIButton) {
+        
+        
+        sender.isSelected = !sender.isSelected
+        
+        if sender.isSelected {
+            passwordTextField.isSecureTextEntry = true
+            showPasswordButton.setImage(UIImage(systemName: "checkmark.square" ), for: .selected)
+        } else {
+            passwordTextField.isSecureTextEntry = false
+            showPasswordButton.setImage(UIImage(systemName: "square"), for: .normal)
+        }
+        
+    }
+    
+    @IBAction func nextButton(_ sender: Any) {
+        guard let welcomeVC = self.storyboard?.instantiateViewController(withIdentifier: "WelcomeViewController") as? WelcomeViewController else {return}
+        
+        welcomeVC.userName = nameTextField.text
+        welcomeVC.modalPresentationStyle = .fullScreen
+        self.present(welcomeVC, animated: true, completion: nil)
+        
+    }
+    
 
 }
