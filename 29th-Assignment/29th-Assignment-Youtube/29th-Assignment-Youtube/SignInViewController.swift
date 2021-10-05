a9f2fb5d81f465728ddec5cef09c21cdfdb1be38
kimsuyeon <81313960+Suyeon9911@users.noreply.github.com>
2021-10-06 07:56:37 +0900

    ✨[FEAT] 회원가입 화면 전환 , 버튼 비활성화 구현

--- 29th-Assignment/29th-Assignment-Youtube/29th-Assignment-Youtube/SignInViewController.swift
+++ 29th-Assignment/29th-Assignment-Youtube/29th-Assignment-Youtube/SignInViewController.swift
@@ -14,20 +14,41 @@ class SignInViewController: UIViewController {
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var nextButton: UIButton!
     
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        nextButton.isEnabled = false
+        self.nameTextField.addTarget(self, action: #selector(self.checkFilledTextField(_:)), for: .editingChanged)
+        self.emailTextField.addTarget(self, action: #selector(self.checkFilledTextField(_:)), for: .editingChanged)
+        self.passwordTextField.addTarget(self, action: #selector(self.checkFilledTextField(_:)), for: .editingChanged)
         // Do any additional setup after loading the view.
     }
     
-    // MARK: IBActions
-    @IBAction func createAccountButton(_ sender: Any) {
+    // MARK: - Methods
+    // MARK: Custom Method
+    
+    @objc func checkFilledTextField(_ sender: Any?) {
         
+        if nameTextField.text != "" && emailTextField.text != "" && passwordTextField.text != "" {
+            nextButton.isEnabled = true
+        } else {
+            nextButton.isEnabled = false
+        }
     }
     
-    @IBAction func nextButton(_ sender: Any) {
+    // MARK: IBActions
+    
+    @IBAction func createAccountButton(_ sender: UIButton) {
+        guard let signUpVC = self.storyboard?.instantiateViewController(withIdentifier: "SignUpViewController") else {return}
+        
+        self.navigationController?.pushViewController(signUpVC, animated: true)
+    }
+    
+    @IBAction func nextButton(_ sender: UIButton) {
+            
         guard let welcomeVC = self.storyboard?.instantiateViewController(withIdentifier: "WelcomeViewController") as? WelcomeViewController else {return}
         
         welcomeVC.userName = nameTextField.text
