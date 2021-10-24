8d96c01a2436888c0d88b2c173738f2bf026cefb
suyeon <rlatndus9911@naver.com>
2021-10-25 04:39:14 +0900

    ➕[ADD] VideoTableViewCell , Data 구조체 파일 추가 (#8)

--- 29th-Assignment/29th-Assignment-Youtube/29th-Assignment-Youtube/Sources/VCs/Youtube/Cells/TableViewCell/VideoTableViewCell.swift
+++ 29th-Assignment/29th-Assignment-Youtube/29th-Assignment-Youtube/Sources/VCs/Youtube/Cells/TableViewCell/VideoTableViewCell.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class VideoTableViewCell: UITableViewCell {
-
+    static let identifier = "VideoTableViewCell"
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var channelImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var moreMenuButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -16,8 +23,13 @@ class VideoTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    }
+    
+    func setData(videoData: VideoData){
+        thumbnailImageView.image = videoData.makeThumbnailImage()
+        channelImageView.image = videoData.makeChannelImage()
+        titleLabel.text = videoData.videoTitle
+        descriptionLabel.text = "\(videoData.channelName) \(videoData.views)회 \(videoData.uploadDate) 전"
     }
     
 }
